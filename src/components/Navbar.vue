218745a7e28125aac701d4a891833963aeaba997
hammadsaeed <asaeed@streamframe.com>
2022-02-02 02:24:57 +0800

    feat: add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+    class="flex-grow-0"
+    :color="this.$vuetify.theme.defaults.font"
+    scroll-target="#scrolling-techniques"
+  >
+    <v-toolbar-title
+      class='headline ml-10 mt-6 black--text text--lighten-0 '
+    >News Dashboard</v-toolbar-title>
+
+    <template v-slot:extension>
+      <v-tabs align-with-title color='secondary'>
+        <v-tab to='/'>List View</v-tab>
+        <v-tab to='/history'>History</v-tab>
+      </v-tabs>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+};
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      primary: '#252422',
+      secondary: '#FFFCF2',
+      accent: '#403D39',
+      font: '#CCC5B9',
+    },
+  },
 });
