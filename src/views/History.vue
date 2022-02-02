bebb2590548d2059d2787f79ed904c736147d651
hammadsaeed <asaeed@streamframe.com>
2022-02-02 14:02:20 +0800

    feat: add history dashboard

---
+++ src/views/History.vue
@@ -0,0 +1,14 @@
+<template>
+  <HistoryDashboard />
+</template>
+
+<script>
+import HistoryDashboard from '../components/HistoryDashboard';
+
+export default {
+  name: 'History',
+  components: {
+    HistoryDashboard,
+  },
+};
+</script>
