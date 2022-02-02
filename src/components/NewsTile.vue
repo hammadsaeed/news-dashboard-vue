cff77d1696672f1f4b5c0388e8cb737782fdbd6d
hammadsaeed <asaeed@streamframe.com>
2022-02-02 22:53:03 +0800

    feat: add error page

--- src/components/NewsTile.vue
+++ src/components/NewsTile.vue
@@ -1,88 +1,86 @@
 <template>
-
-    <v-lazy
-      v-model="isActive"
-      :options="{
-        threshold: .9
-      }"
-      min-height="100"
-      transition="fade-transition"
+  <v-lazy
+    v-model="isActive"
+    :options="{
+      threshold: .9
+    }"
+    min-height="100"
+    transition="fade-transition"
+  >
+    <v-card
+      class="mx-auto mb-4 mx-2"
+      :elevation="hover ? 20 : 2"
+      max-width="300"
+      max-height="350"
+      min-width="300"
+      min-height="350"
+      @mouseover="hover = true" @mouseleave="hover = false"
+      :color='this.$vuetify.theme.defaults.secondary'
     >
-      <v-card
-        class="mx-auto mb-4 mx-2"
-        :elevation="hover ? 20 : 2"
-        max-width="300"
-        max-height="350"
-        min-width="300"
-        min-height="350"
-        @mouseover="hover = true" @mouseleave="hover = false"
-        :color='this.$vuetify.theme.defaults.secondary'
+      <v-img
+        class="align-end cursor: pointer"
+        height="200px"
+        :src="article.urlToImage"
+        style="{cursor: pointer}"
       >
-        <v-img
-          class="align-end cursor: pointer"
-          height="200px"
-          :src="article.urlToImage"
-          style="{cursor: pointer}"
+        <v-expand-transition
+          :href="article.url"
         >
-          <v-expand-transition
-            :href="article.url"
-          >
-            <template>
-              <div
-              class="text-right"
-                v-if="hover"
-              >
-                <v-icon
-                  dark
-                  right
-                  @click="gotoContact()"
-
-                >
-                  mdi-open-in-new
-                </v-icon>
-              </div>
-            </template>
-          </v-expand-transition>
-          <template v-slot:placeholder>
-            <v-row
-              class="fill-height ma-0"
-              align="center"
-              justify="center"
+          <template>
+            <div
+            class="text-right"
+              v-if="hover"
             >
-              <v-progress-circular
-                indeterminate
-                color="black lighten-1"
-              ></v-progress-circular>
-            </v-row>
+              <v-icon
+                dark
+                right
+                @click="gotoContact()"
+
+              >
+                mdi-open-in-new
+              </v-icon>
+            </div>
           </template>
-        </v-img>
-
-        <v-card-text class="text--primary pb-0" min-height="200px">
-          <p
-            class='text-subtitle-2 mb-1 text-wrap'
+        </v-expand-transition>
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
           >
-            {{article.title.split('-').slice(0, -1).join("-")}}
-          </p>
-          <p class='text-subtitle-2 mb-1 text-wrap'>{{article.title.split('-').pop()}}</p >
-        </v-card-text>
+            <v-progress-circular
+              indeterminate
+              color="black lighten-1"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+      <v-card-text class="text--primary pb-0" min-height="200px">
+        <p
+          class='text-subtitle-2 mb-1 text-wrap'
+        >
+          {{getTitle()}}
+        </p>
+        <p class='text-subtitle-2 mb-1 text-wrap'>{{article.title.split('-').pop()}}</p >
+      </v-card-text>
 
-        <v-card-actions class='justify-end'>
-          <v-btn
-            :color='this.$vuetify.theme.defaults.accent'
-            text
-            class='button'
-            :to="{
-              path:
-                `${ type !== 'search' ?
-                  `${type}/${JSON.stringify(`${article.author}-${article.publishedAt}`)}`
-                  : `${JSON.stringify(`${article.author}-${article.publishedAt}`)}`
-                }`,
-            }"
-          >
-            Read More
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-card-actions class='justify-end'>
+        <v-btn
+          :color='this.$vuetify.theme.defaults.accent'
+          text
+          class='button'
+          :to="{
+            path:
+              `${ type !== 'search' ?
+                `${type}/${JSON.stringify(`${article.author}-${article.publishedAt}`)}`
+                : `${JSON.stringify(`${article.author}-${article.publishedAt}`)}`
+              }`,
+          }"
+        >
+          Read More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-lazy>
 </template>
 
@@ -95,6 +93,9 @@ export default {
       const payload = this.article;
       this.$store.dispatch('updateHistory', payload);
       window.open(this.article.url);
+    },
+    getTitle() {
+      return this.article.title.split('-').slice(0, -1).join('-');
     },
   },
   data() {
