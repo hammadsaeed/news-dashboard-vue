a595d22aae5685b5c0daae88db7741729a9c9f05
hammadsaeed <asaeed@streamframe.com>
2022-02-02 19:36:37 +0800

    feat: implement eidtable title

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import state from './state';
 import {
-  getNewsFeed, getSources, updateHistory, getResults,
+  getNewsFeed, getSources, updateHistory, getResults, updateTitle,
 } from './actions';
 import {
-  SET_LIVE_NEWS_FEED, UPDATE_LOADING, SET_SOURCES, INITIALISE, SET_HISTORY, UPDATE_SEACH,
+  SET_LIVE_NEWS_FEED,
+  UPDATE_LOADING,
+  SET_SOURCES,
+  INITIALISE,
+  SET_HISTORY,
+  UPDATE_SEACH,
+  UPDATE_TITLE,
 } from './mutations';
 
 Vue.use(Vuex);
@@ -19,12 +25,14 @@ export default new Vuex.Store({
     INITIALISE,
     SET_HISTORY,
     UPDATE_SEACH,
+    UPDATE_TITLE,
   },
   actions: {
     getNewsFeed,
     getSources,
     updateHistory,
     getResults,
+    updateTitle,
   },
   modules: {
   },
